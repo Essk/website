ace87f10e286c9b5c904b060080503cf22899efe
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2019-03-09 10:22:50 +0000

    npm run build

--- .nuxt/google-analytics.js
+++ .nuxt/google-analytics.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 
 export default async function ({ app: { router } }) {
-  const options = {"dev":true,"debug":{"sendHitTask":false},"id":"UA-65309624-3"}
+  const options = {"dev":true,"debug":{},"id":"UA-65309624-3"}
 
   Vue.use(VueAnalytics, {...{ router }, ...options})
 }
